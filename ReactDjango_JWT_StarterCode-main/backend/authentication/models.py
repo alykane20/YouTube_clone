ba90b4ff74cd9808c2a5ab742bbf98a9ff242a87
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-29 14:36:53 -0400

    uncomment necessary classes

--- ReactDjango_JWT_StarterCode-main/backend/authentication/models.py
+++ ReactDjango_JWT_StarterCode-main/backend/authentication/models.py
@@ -1,9 +1,9 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# class User(AbstractUser):
-#     pass
+class User(AbstractUser):
+    pass
 #     '''
 #     This is a custom version of the built in User class
 #     It contains all of the built in fields and functionality of the standard User
