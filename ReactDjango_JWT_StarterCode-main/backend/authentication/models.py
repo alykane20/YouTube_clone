4d13cb3b1d165221d958ef50c2f08aac667882f0
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-29 14:34:07 -0400

    comment and reply class models initiated by akeem

--- ReactDjango_JWT_StarterCode-main/backend/authentication/models.py
+++ ReactDjango_JWT_StarterCode-main/backend/authentication/models.py
@@ -2,15 +2,26 @@
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
-    pass
-    '''
-    This is a custom version of the built in User class
-    It contains all of the built in fields and functionality of the standard User
-    You can add fields here for any additional properties you want a User to have
-    This is useful for adding roles (Customer and Employee, for example)
-    For just a few roles, adding boolean fields is advised
-    '''
-    # Example (note import of models above that is commented out)
-    # this will add a column to the user table
-    # is_student = models.BooleanField('student status', default=False)
+# class User(AbstractUser):
+#     pass
+#     '''
+#     This is a custom version of the built in User class
+#     It contains all of the built in fields and functionality of the standard User
+#     You can add fields here for any additional properties you want a User to have
+#     This is useful for adding roles (Customer and Employee, for example)
+#     For just a few roles, adding boolean fields is advised
+#     '''
+#     # Example (note import of models above that is commented out)
+#     # this will add a column to the user table
+#     # is_student = models.BooleanField('student status', default=False)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
