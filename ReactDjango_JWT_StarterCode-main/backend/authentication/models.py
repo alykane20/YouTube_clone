58eaa706a207186045aece05828bc3b838f5511d
Devin-Symone Johnson <devinsymone@gmail.com>
2022-04-29 14:48:01 -0400

    create youtube app and moved class models to youtube app created by akeem

--- ReactDjango_JWT_StarterCode-main/backend/authentication/models.py
+++ ReactDjango_JWT_StarterCode-main/backend/authentication/models.py
@@ -15,13 +15,3 @@ class User(AbstractUser):
 #     # this will add a column to the user table
 #     # is_student = models.BooleanField('student status', default=False)
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video_id = models.CharField(max_length=255)
-    text = models.CharField(max_length=255)
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
-class Reply(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
---
+++ ReactDjango_JWT_StarterCode-main/backend/youtube_clone/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+class Reply(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
