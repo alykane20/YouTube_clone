b7710ac3507d6e43863157313346b28a6325db77
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-04-29 17:06:07 -0400

    Added Post function to create a new comment

--- ReactDjango_JWT_StarterCode-main/backend/youtube_clone/views.py
+++ ReactDjango_JWT_StarterCode-main/backend/youtube_clone/views.py
@@ -10,15 +10,24 @@ from .models import Comment
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def get_comments(request):
     if request.method == 'GET':
         comment = Comment.objects.all()
         serializer = CommentSerializer(comment, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
         serializer = CommentSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
