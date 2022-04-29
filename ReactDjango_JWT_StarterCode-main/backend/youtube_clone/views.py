8f83117f4804f0de53d9d9287f6429207ea2de89
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-04-29 16:40:51 -0400

    Completed get all comment fucntionality

--- ReactDjango_JWT_StarterCode-main/backend/youtube_clone/views.py
+++ ReactDjango_JWT_StarterCode-main/backend/youtube_clone/views.py
@@ -6,11 +6,19 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
+from .models import Comment
 
 # Create your views here.
 
-
+@api_view(['GET', 'POST'])
 def get_comments(request):
-    comment = Comment.objects.all()
-    serializer = CommentSerializer(comment, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
