80b904c09feef7b9933573880c46f304f964319b
Devin-Symone Johnson <devinsymone@gmail.com>
2022-05-03 16:52:12 -0400

    starting search page component

--- ReactDjango_JWT_StarterCode-main/frontend/src/components/SearchPage/SearchPage.jsx
+++ ReactDjango_JWT_StarterCode-main/frontend/src/components/SearchPage/SearchPage.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useEffect, useState } from "react/cjs/react.production.min";
 
 
 
 const SearchPage = () => {
-const[]
-
-
-async function videoSearch(){
-    let response = await axios.get('https://www.googleapis.com/youtube/v3/search?q=soccer&key=AIzaSyABXiDhKWUWUuqkgrLUQ4fEq6bTI5A1C9M&part=snippet&type=video&maxResults=4')
+const[video, setVideo] = useState([{ }])
+ useEffect(() => {
+     videoSearch();
+ },[])
+    async function videoSearch(){
+    try{
+        let response = await axios.get('https://www.googleapis.com/youtube/v3/search?q=soccer&key=AIzaSyABXiDhKWUWUuqkgrLUQ4fEq6bTI5A1C9M&part=snippet&type=video&maxResults=4');
+        console.log(response.data)
+        setVideo(response.data)
+} catch(ex) {
+    console.log ('Error');
+  }
 }
 
 
-    return ( 
+    return 
 
-     );
 }
+
  
 export default SearchPage;
+
+
