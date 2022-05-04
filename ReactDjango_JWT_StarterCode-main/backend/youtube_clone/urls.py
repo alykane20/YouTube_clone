8ec68d55775bac2e26c184667dad6b5d84a8087f
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-05-04 11:53:39 -0400

    Added wireframe + changed back URL file from INT to STR

--- ReactDjango_JWT_StarterCode-main/backend/youtube_clone/urls.py
+++ ReactDjango_JWT_StarterCode-main/backend/youtube_clone/urls.py
@@ -4,7 +4,7 @@ from youtube_clone import views
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    path('<int:video_id>/', views.user_comments),
+    path('<str:video_id>/', views.user_comments),
     path('update/<int:pk>/', views.update_comment),
     path('', views.get_comments),
 ]
